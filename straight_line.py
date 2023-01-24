3b70145257d8f5140e839e08d69d980772254486
dn320 <dn320@ic.ac.uk>
2023-01-24 17:37:18 +0000

    add logging for motor status

--- straight_line.py
+++ straight_line.py
@@ -5,18 +5,17 @@ BP = brickpi3.BrickPi3()
 #Using B
 
 def go_straight(value, duration):
-	# start_time = time.time()
-	# while time.time() - start_time <= duration:
-	# 	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
-	# BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, value)
-	time.sleep(duration)
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
+		start_time = time.time()
+  	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
+		while time.time() - start_time <= duration:
+				(status, power, degrees, velocity) = BP.get_motor_status(BP.PORT_B)
+	  		print(f"degrees: {degrees}, velocity: {velocity}")
+		BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
 
-
-go_straight(10, 5)
+try:
+		go_straight(10, 5)
 
 
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all() 
+  	BP.reset_all() 
