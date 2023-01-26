b586fe3dd4e844c68420b4df069cbc931c1695f9
dn320 <dn320@ic.ac.uk>
2023-01-26 15:42:45 +0000

    fix func

--- straight_line.py
+++ straight_line.py
@@ -5,12 +5,10 @@ BP = brickpi3.BrickPi3()
 #Using B
 
 def go_straight(num_rotations):
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
 	BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + num_rotations*360)
 	BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) + num_rotations*360)
 	
 def rotate90():
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
 	BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + 244)
 	BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) - 244)
 
@@ -29,13 +27,10 @@ def drawX():
  #to do calibrate and find optimal values for go_straight and rotate90
 
 try:
-	# go_straight(1)
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 5)
-	time.sleep(5)
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
-
-
-
+	BP.set_motor_limits(BP.PORT_B + BP.PORT_C, 50, 200)
+	go_straight(0.5)
+	rotate90()
+	go_straight(0.5)
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
   	BP.reset_all() 
