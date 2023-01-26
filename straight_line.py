d75a0a558146a37bf745c6e99695e414dbfb2daa
dn320 <dn320@ic.ac.uk>
2023-01-26 14:42:36 +0000

    change implementation to number of rotations instead of time

--- straight_line.py
+++ straight_line.py
@@ -4,14 +4,11 @@ BP = brickpi3.BrickPi3()
 
 #Using B
 
-def go_straight(value, duration):
-	start_time = time.time()
-	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
-	while time.time() - start_time <= duration:
-			(status, power, degrees, velocity) = BP.get_motor_status(BP.PORT_B)
-			print(f"degrees: {degrees}, velocity: {velocity}")
+def go_straight(num_rotations):
 	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
-
+	BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + num_rotations*360)
+	BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) + num_rotations*360)
+	
 def rotate90():
 	BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, 0)
 	BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + 244)
@@ -32,7 +29,7 @@ def drawX():
  #to do calibrate and find optimal values for go_straight and rotate90
 
 try:
-	rotate90()
+	go_straight(1)
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
   	BP.reset_all() 
