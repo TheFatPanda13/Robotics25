0c5bbf58ac4f2dca421be424ac4919248e9e107b
TheFatPanda13 <eamgupta@gmail.com>
2023-01-26 18:12:53 +0000

    adjust power+angle

--- calibration_rotation.py
+++ calibration_rotation.py
@@ -6,15 +6,15 @@ BP = brickpi3.BrickPi3()
 def drawSquare(controller):
     for i in range(4):
         controller.go_straight(0.20)
-        time.sleep(2)
+        time.sleep(1)
         rotate90(BP)
-        time.sleep(2)
+        time.sleep(5)
 
 def rotate90(BP):
-    BP.set_motor_limits(BP.PORT_B, power=50, dps=40)
-    BP.set_motor_limits(BP.PORT_C, power=50, dps=40)
-    BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + 600)
-    BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) - 600)
+    BP.set_motor_limits(BP.PORT_B, power=70, dps=60)
+    BP.set_motor_limits(BP.PORT_C, power=70, dps=60)
+    BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + 300)
+    BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) - 300)
 
 
 try:
