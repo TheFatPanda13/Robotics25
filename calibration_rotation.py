a47787bf59f7ec6764b7b4a5f9ff247d0e900330
TheFatPanda13 <eamgupta@gmail.com>
2023-01-26 17:51:47 +0000

    adjust_rot_speed

--- calibration_rotation.py
+++ calibration_rotation.py
@@ -6,9 +6,12 @@ BP = brickpi3.BrickPi3()
 def drawSquare(controller):
     for i in range(4):
         controller.go_straight(20)
+        time.sleep(2)
         rotate90(BP)
+        time.sleep(2)
 
 def rotate90(BP):
+    BP.set_motor_limits(BP.PORT_B, BP_PORT_C, power=20, dps=40)
 	BP.set_motor_position(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B) + 244)
 	BP.set_motor_position(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C) - 244)
 
