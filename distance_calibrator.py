4986aa01fa544fcfa9470d30e20263d40dfdd1bb
cam-jw <cam.jw@outlook.com>
2023-01-26 16:44:24 +0000

    add a test distance calibrator

---
+++ distance_calibrator.py
@@ -0,0 +1,17 @@
+import brickpi3, time, numpy
+from control_test import Controller
+
+BP = brickpi3.BrickPi3()
+
+try:
+	controller = Controller(BP)
+	for distance in [5, 10, 15, 20, 25]:
+		controller.go_straight(distance)
+		time.sleep(30)
+    controller.BP.reset_all()
+
+except KeyboardInterrupt: 
+    BP.reset_all() 
+
+
+
