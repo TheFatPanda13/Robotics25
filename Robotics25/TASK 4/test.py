6f8fe8f746d617a427a9b1a0c29777c72ef183e2
TheFatPanda13 <eamgupta@gmail.com>
2023-02-22 10:21:40 +0000

    map inverter

--- Robotics25/TASK 4/test.py
+++ Robotics25/TASK 4/test.py
@@ -8,5 +8,8 @@ edges = [[[0,0], [0,168]], [[0,168], [84, 168]], \
     [[84, 126], [84, 210]], [[84, 210], [168, 210]], \
          [[168, 210], [168, 84]], [[168, 84], [210, 84]], \
              [[210, 84], [210, 0]]]
+
+map_inverter = lambda x: [x[0][::-1], x[1][::-1]]
+new_edges = [list(map(map_inverter, x) for x in edges)]
 env = Map(edges)
 env.draw_walls()
